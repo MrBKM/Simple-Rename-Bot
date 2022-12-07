b5a69c4745f7e19ba122d9a4e0e68c8c843e0059
BKM TG <119480777+MrBKM@users.noreply.github.com>
2022-12-07 09:31:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+API_ID = int(environ.get("API_ID", "12694006"))
+API_HASH = environ.get("API_HASH", "8719d11809492f836004a39b42599215")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5861466478:AAFNiWePC5pUvXEKFnK9_JhiSZmOX3Fiwcs")
+ADMIN = int(environ.get("ADMIN", "953377581"))          
 CAPTION = environ.get("CAPTION", "")
 
 class temp(object):
